da8a7c546226896f8df7cb131becc9176c0ccbfa
JoshuaAv07 <a6520150001@utch.edu.mx>
2022-05-27 16:25:19 -0600

    product and costumer RestController

--- src/main/java/com/example/test/restcontrollers/CostumerRestController.java
+++ src/main/java/com/example/test/restcontrollers/CostumerRestController.java
@@ -1,9 +1,7 @@
 package com.example.test.restcontrollers;
 
 import com.example.test.entities.CostumerEntity;
-import com.example.test.entities.UserEntity;
 import com.example.test.services.CostumerService;
-import com.example.test.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
